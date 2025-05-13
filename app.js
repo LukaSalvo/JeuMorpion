926218fbd99e65105ff0fa75164105346de27e3c
AminoBela <abelaliabendjafar@gmail.com>
2025-05-13 15:30:49 +0200

    configs EJSet ajout dnas le gitignore de node_modules

--- app.js
+++ app.js
@@ -10,3 +10,8 @@ app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
 
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { message: 'Hello World' });
+});
